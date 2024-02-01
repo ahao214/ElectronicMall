d06b364f0a70ddf0aa9f522e1bfc03d144bbbf2a
liupenghao@fulan.com.cn <haochen214@163.com>
2024-02-01 21:22:40 +0800

    树形实体接口

---
+++ ElectronicMall.Domain.Abstractions/Entities/ITreeEntity.cs
@@ -0,0 +1,27 @@
+﻿/*
+Desc:树形实体接口
+Author:Joker
+CreatedDate:24-02-01
+*/
+
+namespace ElectronicMall.Domain.Abstractions.Entities
+{
+    /// <summary>
+    /// 树形实体接口
+    /// </summary>
+    /// <typeparam name="T">实体类型</typeparam>
+    public interface ITreeEntity<T> : IEntity, ITree<T>
+    {
+    }
+
+
+    public interface ITreeEntity<TKey, TEntity> : ITreeEntity<TEntity>, IEntity<TKey> where TKey : struct, IEquatable<TKey>
+        where TEntity : ITreeEntity<TKey, TEntity>
+    {
+        /// <summary>
+        /// 父节点Id
+        /// </summary>
+        TKey? ParentId { get; set; }
+    }
+
+}
