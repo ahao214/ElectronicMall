add06d1545fc7c1ae6b8bb729b97ef5b817b3501
liupenghao@fulan.com.cn <haochen214@163.com>
2024-02-01 22:36:13 +0800

    仓储接口实现

---
+++ ElectronicMall.Domain.Repository.EntityFrameworkCore/GlobalImports.cs
@@ -0,0 +1,9 @@
+﻿/*
+Desc:EF Core 仓储 全局命名空间
+Author:Joker
+CreatedDate:24-02-01
+*/
+
+global using ElectronicMall.Domain.Abstractions.Entities;
+global using ElectronicMall.Domain.Abstractions.Repositories;
+global using Microsoft.EntityFrameworkCore;
