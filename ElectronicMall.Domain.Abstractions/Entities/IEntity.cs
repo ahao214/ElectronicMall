d63dc33e945738984ddb49bcaeab750eb9834378
liupenghao@fulan.com.cn <haochen214@163.com>
2024-02-01 21:13:05 +0800

    基础实体接口

---
+++ ElectronicMall.Domain.Abstractions/Entities/IEntity.cs
@@ -0,0 +1,28 @@
+﻿/*
+Desc:基础实体接口
+Author:Joker
+CreatedDate:24-02-01
+*/
+
+namespace ElectronicMall.Domain.Abstractions.Entities
+{
+    /// <summary>
+    /// 实体接口
+    /// </summary>
+    public interface IEntity
+    {
+    }
+
+    /// <summary>
+    /// 实体接口
+    /// </summary>
+    /// <typeparam name="TKey">唯一标识的类型</typeparam>
+    public interface IEntity<TKey> : IEntity where TKey : IEquatable<TKey>
+    {
+        /// <summary>
+        /// 实体的唯一标识
+        /// </summary>
+        TKey Id { get; set; }
+    }
+
+}
